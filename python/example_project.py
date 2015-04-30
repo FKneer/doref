b930cd09237ee5cbbb87c39c16bc1a2303e04583
Fabian Kneer <fabian.kneer@fh-dortmund.de>
2015-04-30 11:57:01 +0200

    new Test - distributed teamwork - Splitting a project file into sub files

--- python/example_project.py
+++ python/example_project.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from example_method import *
-from plantuml import *
+from _test_project.structur import *
 #------------------------------------------------------------
 # example_project.py
 # Smart vacuum cleaner
@@ -11,108 +10,7 @@ from plantuml import *
 
 # Example RE Sceanrio: Stakeholder have conflicts
 
-w = World("My Smart Home")
-cd("./-")
-s = System("Vacuum Cleaner")
-cd("./-")
-p = Project("Adaptive Control")
+node("/").dump()
 
-
-
-cd("/*/Adaptive Control")
-Folder("Protocols")
-Folder("Specifications")
-cd("./-")
-
-IEEE830SRS("Software Requirements for Adaptive Control of Vacuum Cleaner",
-           [["Erik Kamsties and Fabian Kneer", "RE Research Group", "FH Dortmund"]],
-           {'language': 'german', 'paper': 'a4', 'font': '11pt'})
-
-
-cd("/*/Specific Requirements")
-
-Chapter("Customer Requirements")
-cd("./-")
-
-Req("Clean at night",
-    "The robot shall clean the appartment at night.",
-    {'Priority': 1,
-     'Effort': 20,
-     'Optional': 1})
-
-Req("Power",
-    "The suction power must not exceed a given threshold.",
-    {'Priority': 1})
-
-
-Req("Silence", "The operation of the vacuum cleaner should be as silent as possible.",
-    {'Priority': 2,
-     'Trace': [node("./*/Power"), node("./*/Clean")]})
-cd("./-")
-Inf("The vacuum cleaner should not disturb the persons living in the apartment.")
-cd("..")
-
-
-Inf("Porto (port. Aussprache ['poɾtu]) ist die "
-    "Hauptstadt des gleichnamigen Distriktes in "
-    "Portugal und liegt am Douro vor dessen Mündung "
-    "in den Atlantischen Ozean. Mit circa 237.000 Einwohnern "
-    "(port. portuenses; dt. Portuenser genannt) ist sie "
-    "nach Lissabon und ihrer Nachbarstadt Vila Nova de Gaia "
-    "die drittgrößte Stadt des Landes. ")
-
-
-Inf('''Der rund 1,29 Millionen[3]
-    Einwohner zählende Ballungsraum Grande Porto rangiert
-    landesweit jedoch an zweiter Stelle. Porto ist eine der ältesten
-    europäischen Städte, sie wird gern als die „Hauptstadt
-    des Nordens“ bezeichnet.''')
-
-
-Figure("RE - World, Documents, and Workflows", './_example_project/model.png',
-       "This figure shows the main corner stones of RE.",
-       {'size': 'fit'})
-
-
-Table("Actor Description",
-      [
-          ['Term', 'Description'],
-          ['Customer-Relationship-Management',
-           'Customer-Relationship-Management, kurz CRM (dt. Kundenbeziehungsmanagement) '
-           'oder Kundenpflege, bezeichnet die konsequente Ausrichtung einer '
-           'Unternehmung auf ihre Kunden und die systematische Gestaltung der '
-           'Kundenbeziehungsprozesse. Die dazugehörende Dokumentation und Verwaltung '
-           'von Kundenbeziehungen ist ein wichtiger Baustein und ermöglicht ein '
-           'vertieftes Beziehungsmarketing.  ']
-      ], "This table defines some terms.")
-
-
-cd("/*/Specific Requirements")
-
-Chapter("Models")
-cd("./-")
-
-PlantUML("UML Example","""
-Alice -> Bob: A
-Bob --> Alice: B
-Alice -> Bob: C
-Bob --> Alice: D
-""", "Example: plantUML Seq-Diagramm")
-
-
-GenericModel("States of a Traffic Light", "This model shows the states of a traffic light in Germany.")
-cd('./-')
-Circle("Red")
-Circle("Red-yellow")
-Circle("Green")
-Circle("Yellow")
-
-Arrow(node('./*/Red'), node('./*/Red-yellow'))
-Arrow(node('./*/Red-yellow'), node('./*/Green'))
-Arrow(node('./*/Green'), node('./*/Yellow'))
-Arrow(node('./*/Yellow'), node('./*/Red'))
-
-w.dump()
-
-w.genPDF()
-w.genHTML(["ref", "ieee830"])
+node("/").genPDF()
+node("/").genHTML(["ref", "ieee830"])
