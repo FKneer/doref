d1eae7bfa23c997e16ff893997ccab1dbf2733ed
Fabian Kneer <fabian.kneer@fh-dortmund.de>
2015-04-30 11:57:30 +0200

    new Test - distributed teamwork - Splitting a project file into sub files

---
+++ python/_test_project/chapter2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from _test_project.structur import *
+
+cd("/*/Models")
+
+PlantUML("UML Example","""
+Alice -> Bob: A
+Bob --> Alice: B
+Alice -> Bob: C
+Bob --> Alice: D
+""", "Example: plantUML Seq-Diagramm")
+
+
+GenericModel("States of a Traffic Light", "This model shows the states of a traffic light in Germany.")
+cd('./-')
+Circle("Red")
+Circle("Red-yellow")
+Circle("Green")
+Circle("Yellow")
+
+Arrow(node('./*/Red'), node('./*/Red-yellow'))
+Arrow(node('./*/Red-yellow'), node('./*/Green'))
+Arrow(node('./*/Green'), node('./*/Yellow'))
+Arrow(node('./*/Yellow'), node('./*/Red'))
